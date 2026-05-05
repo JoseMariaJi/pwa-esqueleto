e6e376b0d41aa3f36dd3dc5bb54f1cc9da74a13e
José <jmjime@gmail.com>
2026-05-05 22:08:22 +0200

    Update PWA

--- appvideo.js
+++ appvideo.js
@@ -63,7 +63,7 @@ async function configurarModoGrabacion() {
         stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
         preview.srcObject = stream;
         preview.style.display = "block"; // Mostramos el recuadro de la cámara
-         hacerMovible(dpreview);
+         hacerMovible(preview);
         // El botón iniciará la reproducción y la grabación simultánea
         btnMain.onclick = comenzarGrabacionYVideo;
 
--- version-actual.js
+++ version-actual.js
@@ -1,1 +1,1 @@
-const APP_VERSION = "5/5/2026, 22:05:59"
+const APP_VERSION = "5/5/2026, 22:08:22"
